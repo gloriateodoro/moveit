a595dfe5f71329fba221784892a61a273cc91d7b
gloriateodoro <gloriateodoro1@gmail.com>
2021-02-27 22:04:40 -0300

    Deletados

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-
-ReactDOM.render(
-<App />, //JSX / sintaxe próxima de XTML ou XML dentro do javascript
-document.getElementById('root')
-); 
