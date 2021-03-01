f558f958c3f9a6029e29cb51bea2ec9d0e8a23d7
gloriateodoro <gloriateodoro1@gmail.com>
2021-03-01 01:06:33 -0300

    Arquivos tsx

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/global.css'
+
+import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext'
+import { useState } from 'react';
 //reaproveitar coisas que se repetem, ficam por volta da aplicação
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />       
+    </ChallengesProvider>
+      
+    
+  )  
 }
 
 export default MyApp
